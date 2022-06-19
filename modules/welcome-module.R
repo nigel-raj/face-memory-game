bf63dcb430ad434c2aaf129444ed1f7e2d67aa87
nigel-raj <76651529+nigel-raj@users.noreply.github.com>
2022-06-19 14:46:07 +0800

    Update welcome-module.R

--- modules/welcome-module.R
+++ modules/welcome-module.R
@@ -12,7 +12,7 @@ welcome_UI <- function(id) {
     ),
 
     setBackgroundImage(src = "https://images.unsplash.com/photo-1579546929518-9e396f3cc809?ixlib=rb-1.2.1&w=1080&fit=max&q=80&fm=jpg&crop=entropy&cs=tinysrgb", shinydashboard = FALSE),
-    tags$div(tags$h4(textInput("nama","Enter Name:",""), sliderInput("timeLimit", "Time limit:",
+    tags$div(tags$h4(textInput("nama","Enter Name:",""), sliderInput("timeLimit", "Time limit (seconds):",
                                                                      min = 0, max = 100,
                                                                      value = 70), style = "text-align: justify;", "About:"),
                          
